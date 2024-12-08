759f9700a5468ceb70730a5834bbe5a10d827d47
Ronilson da Conceição Batista <ronilsondaconceicao@gmail.com>
2024-12-08 10:58:58 -0300

    cadastrando produto

--- at.py
+++ at.py
@@ -1,9 +1,9 @@
 # Definir o estoque inicial como string
 estoque_inicial = "Notebook Dell;201;15;3200.00;4500.00#Notebook Lenovo;202;10;2800.00;4200.00#Mouse Logitech;203;50;70.00;150.00#Mouse Razer;204;40;120.00;250.00#Monitor Samsung;205;10;800.00;1200.00#Monitor LG;206;8;750.00;1150.00#Teclado Mecânico Corsair;207;30;180.00;300.00#Teclado Mecânico Razer;208;25;200.00;350.00#Impressora HP;209;5;400.00;650.00#Impressora Epson;210;3;450.00;700.00#Monitor Dell;211;12;850.00;1250.00#Monitor AOC;212;7;700.00;1100.00"
+produtos = []
 
 # Função para tranformar a string do estoque inicial em uma lista de dicionario
 def carregar_estoque_inicial(estoque_str):
-    produtos = []
     items = estoque_str.split("#")  
     # Separar produto por marcação #
     for item in items:
@@ -18,6 +18,27 @@ def carregar_estoque_inicial(estoque_str):
         produtos.append(produto)
     return produtos
 
+# Função para cadastrar um novo produto
+def cadastrar_produto(produtos):
+    print("***Cadastro de Novo Produto***")
+    descricao = input("Descrição: ")
+    id = int(input("Código (único): "))
+    quantidade = int(input("Quantidade em estoque: "))
+    custo = float(input("Custo do item: "))
+    preco_venda = float(input("Preço de venda por item: "))
+    
+    novo_produto = {
+        "descricao": descricao,
+        "codigo": id,
+        "quantidade": quantidade,
+        "custo": custo,
+        "preco_venda": preco_venda
+    }
+    produtos.append(novo_produto)
+    print("Produto cadastrado com sucesso!")
+
+
+estoque = carregar_estoque_inicial(estoque_inicial)
 
 while True:
     print("\n ***Sistema de Gerenciamento de Estoque***")
@@ -29,7 +50,7 @@ while True:
     if opcao == "1":
         break
     if opcao == "2":
-        break
+        cadastrar_produto(estoque)
     elif opcao == "3":
         print("Saindo. Até!")
         break
